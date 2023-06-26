e5d0c06de696ecb1ec99ee8817f549c4cc6b1136
ikkai-r <rm.sales92@gmail.com>
2023-06-26 12:53:33 +0800

    modified:   css/style.css 	modified:   edit-post.html 	modified:   index.html 	modified:   js/post.js 	modified:   post.html

--- js/post.js
+++ js/post.js
@@ -12,9 +12,13 @@ document.getElementById("add-tag-area").addEventListener("keydown", function(e) 
     }
   });
   
-  function createNewDiv(text) {
+function createNewDiv(text) {
     var newDiv = document.createElement("div");
     newDiv.textContent = text;
-    newDiv.className = "postcom-tags post-tags-cont mt-2";
+    newDiv.className = "postcom-tags post-tags-cont mt-2 added-tag";
     document.getElementById("tag-grp").appendChild(newDiv);
-  }
+}
+
+$(document).on("click", ".fa-xmark", function () {
+  $(this).closest("div").find(".added-tag").css("background-color", "red");
+}); 
