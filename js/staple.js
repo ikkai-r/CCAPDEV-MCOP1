b4fff8e83d340b3347a69c8edd9ee283fe0b0ba8
ikkai-r <rm.sales92@gmail.com>
2023-06-26 20:39:10 +0800

    modified:   js/staple.js

--- js/staple.js
+++ js/staple.js
@@ -1,7 +1,7 @@
 $(".add-tag").click(function () {
     if($(this).hasClass('fa-circle-plus')) {
         $(this).removeClass('fa-circle-plus');
-        $(this).addClass('fa-circle-down');
+        $(this).addClass('fa-circle-minus');
     } else {
         $(this).removeClass('fa-circle-minus');
         $(this).addClass('fa-circle-plus');
